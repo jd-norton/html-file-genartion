e340ed8177e8bfec51f512f8a9e710429ae4c0a2
jd-norton <jacob.daniel.norton@gmail.com>
2022-12-03 03:01:46 -0600

    Delete test.html Added file path options for css, js and output html file

--- generate.bat
+++ generate.bat
@@ -1,13 +1,18 @@
 @ECHO OFF
 
-FOR /F "tokens=* delims=" %%x in (start-gen.html) DO echo %%x > test.html
+set cssPath=style.css
+set jsPath=script.js
+
+set indexPath=index.html
+
+FOR /F "tokens=* delims=" %%x in (start-gen.html) DO echo %%x > %indexPath%
 
 :: write css to file
-FOR /F "tokens=* delims=" %%x in (style.css) DO echo %%x >> test.html
+FOR /F "tokens=* delims=" %%x in (%cssPath%) DO echo %%x >> %indexPath%
 
-FOR /F "tokens=* delims=" %%x in (middle-gen.html) DO echo %%x >> test.html
+FOR /F "tokens=* delims=" %%x in (middle-gen.html) DO echo %%x >> %indexPath%
 
 :: write js to file
-FOR /F "tokens=* delims=" %%x in (script.js) DO echo %%x >> test.html
+FOR /F "tokens=* delims=" %%x in (%jsPath%) DO echo %%x >> %indexPath%
 
-FOR /F "tokens=* delims=" %%x in (end-gen.html) DO echo %%x >> test.html
+FOR /F "tokens=* delims=" %%x in (end-gen.html) DO echo %%x >> %indexPath%
