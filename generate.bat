52b0b3cfdd4b8944cb5f7d44359cbb47e154e193
jd-norton <jacob.daniel.norton@gmail.com>
2022-12-07 01:30:06 -0600

    Added comment explaining new output file generation

--- generate.bat
+++ generate.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
 
-:: TODO: Variable to When to stop for start-gen, start and stop for middle-gen, and start for end-gen
+:: TODO: Loop through starting html file. Loop on it and add lines to output file until find line with "<head>" or where want to insert css. Loop and insert start and end "<styles>" and css file. Then go back to inserting html file until find line last line before "</body>" and insert start and end "<script>" and js file. Then finish inserting html into output. Done
 
+set "middleGenStart=    <body>"
+set "middleGenEnd=        </div>"
+set inRange=false
+
+set middleGenPath=..\..\color-progressive\index.html
 set cssPath=..\..\color-progressive\styles.css
 set jsPath=..\..\color-progressive\script.js
 
@@ -14,7 +19,18 @@ FOR /F "tokens=* delims=" %%x in (start-gen.html) DO echo %%x >> %indexPath%
 :: write css to file
 FOR /F "tokens=* delims=" %%x in (%cssPath%) DO echo %%x >> %indexPath%
 
-FOR /F "tokens=* delims=" %%x in (middle-gen.html) DO echo %%x >> %indexPath%
+FOR /F "tokens=* delims=" %%x in (pre-middle-gen.html) DO echo %%x >> %indexPath%
+
+
+FOR /F "tokens=* delims=" %%x in (%middleGenPath%) DO (
+
+    IF %%x == middleGenStart set inRange=true
+
+    IF %%x == middleGenEnd set inRange=false
+    
+    IF %inRange% == "true" echo %%x >> %indexPath%
+    ::ECHO is OFF??? not working
+)
 
 :: write js to file
 FOR /F "tokens=* delims=" %%x in (%jsPath%) DO echo %%x >> %indexPath%
